4ec31acaba7bb36311d50205997f70f354c06684
Ricardo Coronado <rikardo.corp@gmail.com>
2017-05-06 13:46:04 -0500

    alex

---
+++ nets/net_alex.py
@@ -0,0 +1,195 @@
+import os
+import tensorflow as tf
+import numpy as np
+import time
+import inspect
+
+VGG_MEAN = [103.939, 116.779, 123.68]
+
+
+class VGG19:
+
+    def __init__(self, npy_path=None, trainable=True, learning_rate=0.05, dropout=0.5, load_weight_fc=False):
+        if npy_path is not None:
+            self.data_dict = np.load(npy_path, encoding='latin1').item()
+            print("npy file loaded")
+        else:
+            self.data_dict = None
+            print("random weight")
+
+        self.var_dict = {}
+        self.trainable = trainable
+        self.learning_rate = learning_rate
+        self.dropout = dropout
+        self.load_weight_fc = load_weight_fc
+
+    def build(self, rgb, target, train_mode=True, size_layer_fc=4096, num_class=1000):
+
+        #
+        # 2-D is CONVERTED TO SQUARE TENSOR.
+        # ---------------------------------
+
+        if len(rgb.get_shape()) == 2:
+            x_dim = int(np.sqrt(rgb.get_shape().as_list()[1]))
+            x_tensor = tf.reshape(rgb, [-1, x_dim, x_dim, 1])
+
+
+        x_tensor = tf.image.resize_images(x_tensor, [224, 224])
+        xdim = 224
+        start_time = time.time()
+        print("build model started")
+        rgb_scaled = rgb * 255.0
+
+        # Convert RGB to BGR
+        bgr = tf.concat(axis=3, values=[
+            x_tensor,
+            x_tensor,
+            x_tensor,
+        ])
+
+        assert bgr.get_shape().as_list()[1:] == [xdim, xdim, 3]
+
+        self.conv1_1 = self.conv_layer(bgr, 3, 64, "conv1_1")
+        self.conv1_2 = self.conv_layer(self.conv1_1, 64, 64, "conv1_2")
+        self.pool1 = self.max_pool(self.conv1_2, 'pool1')
+
+        self.conv2_1 = self.conv_layer(self.pool1, 64, 128, "conv2_1")
+        self.conv2_2 = self.conv_layer(self.conv2_1, 128, 128, "conv2_2")
+        self.pool2 = self.max_pool(self.conv2_2, 'pool2')
+
+        self.conv3_1 = self.conv_layer(self.pool2, 128, 256, "conv3_1")
+        self.conv3_2 = self.conv_layer(self.conv3_1, 256, 256, "conv3_2")
+        self.conv3_3 = self.conv_layer(self.conv3_2, 256, 256, "conv3_3")
+        self.conv3_4 = self.conv_layer(self.conv3_3, 256, 256, "conv3_4")
+        self.pool3 = self.max_pool(self.conv3_4, 'pool3')
+
+        self.conv4_1 = self.conv_layer(self.pool3, 256, 512, "conv4_1")
+        self.conv4_2 = self.conv_layer(self.conv4_1, 512, 512, "conv4_2")
+        self.conv4_3 = self.conv_layer(self.conv4_2, 512, 512, "conv4_3")
+        self.conv4_4 = self.conv_layer(self.conv4_3, 512, 512, "conv4_4")
+        self.pool4 = self.max_pool(self.conv4_4, 'pool4')
+
+        self.conv5_1 = self.conv_layer(self.pool4, 512, 512, "conv5_1")
+        self.conv5_2 = self.conv_layer(self.conv5_1, 512, 512, "conv5_2")
+        self.conv5_3 = self.conv_layer(self.conv5_2, 512, 512, "conv5_3")
+        self.conv5_4 = self.conv_layer(self.conv5_3, 512, 512, "conv5_4")
+        self.pool5 = self.max_pool(self.conv5_4, 'pool5')
+
+        self.fc6 = self.fc_layer(self.pool5, 25088, 4096, "fc6", load_weight_force=True) # 25088 = ((224 // (2 ** 5)) ** 2) * 512
+        self.relu6 = tf.nn.relu(self.fc6)
+
+        # DROPOUT
+        if self.trainable is True:
+            self.relu6 = tf.cond(train_mode, lambda: tf.nn.dropout(self.relu6, self.dropout), lambda: self.relu6)
+
+        self.fc7 = self.fc_layer(self.relu6, 4096, 4096, "fc7", load_weight_force=True)
+        self.relu7 = tf.nn.relu(self.fc7)
+
+        # DROPOUT
+        if self.trainable is True:
+            self.relu7 = tf.cond(train_mode, lambda: tf.nn.dropout(self.relu7, self.dropout), lambda: self.relu7)
+
+        self.fc8 = self.fc_layer(self.relu7, 4096, 128, "fc8")
+        self.relu8 = tf.nn.relu(self.fc8)
+
+        self.fc9 = self.fc_layer(self.relu7, 128, 10, "fc9")
+        self.prob = tf.nn.softmax(self.fc9, name="prob")
+
+        # COST - TRAINING
+        self.cost = tf.reduce_mean((self.prob - target) ** 2)
+        # self.train = tf.train.AdamOptimizer(self.learning_rate).minimize(self.cost)
+        self.train = tf.train.GradientDescentOptimizer(self.learning_rate).minimize(self.cost)
+
+        self.data_dict = None
+        print(("build model finished: %ds" % (time.time() - start_time)))
+
+    # Layer MaxPool
+    def max_pool(self, bottom, name):
+        return tf.nn.max_pool(bottom, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME', name=name)
+
+    # Layer Convolutional
+    def conv_layer(self, bottom, in_channels, out_channels, name):
+        with tf.variable_scope(name):
+            filt, conv_biases = self.get_conv_var(3, in_channels, out_channels, name)
+
+            conv = tf.nn.conv2d(bottom, filt, [1, 1, 1, 1], padding='SAME')
+            bias = tf.nn.bias_add(conv, conv_biases)
+            relu = tf.nn.relu(bias)
+            return relu
+
+    # Generate Parameter Convol layer
+    def get_conv_var(self, filter_size, in_channels, out_channels, name):
+        initial_value = tf.truncated_normal([filter_size, filter_size, in_channels, out_channels], 0.0, 0.001)
+        filters = self.get_var_conv(initial_value, name, 0, name + "_filters")
+
+        initial_value = tf.truncated_normal([out_channels], .0, .001)
+        biases = self.get_var_conv(initial_value, name, 1, name + "_biases")
+
+        return filters, biases
+
+    # Construct dictionary with random parameters or load parameters
+    def get_var_conv(self, initial_value, name, idx, var_name):
+        if self.data_dict is not None and name in self.data_dict:
+            value = self.data_dict[name][idx]
+        else:
+            value = initial_value
+
+        if self.trainable:
+            var = tf.Variable(value, name=var_name)
+        else:
+            var = tf.constant(value, dtype=tf.float32, name=var_name)
+
+        self.var_dict[(name, idx)] = var
+        assert var.get_shape() == initial_value.get_shape()
+        return var
+
+    # Layer FullConnected
+    def fc_layer(self, bottom, in_size, out_size, name, load_weight_force=False):
+        with tf.variable_scope(name):
+            weights, biases = self.get_fc_var(in_size, out_size, name, load_weight_force)
+
+            x = tf.reshape(bottom, [-1, in_size])
+            fc = tf.nn.bias_add(tf.matmul(x, weights), biases)
+            return fc
+
+    # Generate Parameter FullConnect layer
+    def get_fc_var(self, in_size, out_size, name, load_wf=False):
+        initial_value = tf.truncated_normal([in_size, out_size], 0.0, 0.001)
+        weights = self.get_var_fc(initial_value, name, 0, name + "_weights", load_wf)
+
+        initial_value = tf.truncated_normal([out_size], .0, .001)
+        biases = self.get_var_fc(initial_value, name, 1, name + "_biases", load_wf)
+
+        return weights, biases
+
+    # Construct dictionary with random parameters or load parameters
+    def get_var_fc(self, initial_value, name, idx, var_name, load_wf=False):
+        if self.data_dict is not None and name in self.data_dict and ((self.load_weight_fc is True) or (load_wf is True)):
+            value = self.data_dict[name][idx]
+        else:
+            value = initial_value
+
+        if self.trainable:
+            var = tf.Variable(value, name=var_name)
+        else:
+            var = tf.constant(value, dtype=tf.float32, name=var_name)
+
+        self.var_dict[(name, idx)] = var
+        assert var.get_shape() == initial_value.get_shape()
+        return var
+
+    # Save weight model
+    def save_npy(self, sess, npy_path="./vgg19-save.npy"):
+        assert isinstance(sess, tf.Session)
+
+        data_dict = {}
+
+        for (name, idx), var in list(self.var_dict.items()):
+            var_out = sess.run(var)
+            if name not in data_dict:
+                data_dict[name] = {}
+            data_dict[name][idx] = var_out
+
+        np.save(npy_path, data_dict)
+        print("File saved", npy_path)
+        return npy_path
---
+++ tester/tester_alex.py
@@ -0,0 +1,174 @@
+"""
+Expert tester for the vgg19_trainable
+
+:param load_weight_fc
+# Variable para cargar los pesos de la capa fullConnect
+# False : Cuando modificamos las capas fc para que el modelo clasifique un nuevo tipo de datos
+# True  : Cuando ya contamos con un archivo de pesos entrenados .npy de la nueva red podemos cargarlos
+# Nota, siempre que utilicemos inicialmente los pesos originales del archivo vgg19.npy debemos setear la variable en False
+# ya que este archivo almacena los pesos de la red vgg original, al cargarlos en nuestra red ocurrira un error.
+
+"""
+import time
+import tensorflow as tf
+import sys, os
+import numpy as np
+
+switch_server = True
+
+testdir = os.path.dirname('__file__')
+srcdir = '..'
+sys.path.insert(0, os.path.abspath(os.path.join(testdir, srcdir)))
+
+if switch_server is True:
+    from tools import utils
+    from nets import net_alex as ALEX
+    from tools.dataset_image import Dataset
+else:
+    from tensorflow_manage_nets.tools import utils
+    from tensorflow_manage_nets.nets import net_alex as ALEX
+    from tensorflow_manage_nets.tools.dataset_image import Dataset
+
+# ..................................................................
+
+# GLOBAL VARIABLES
+path = '../../data/ISB2016/'
+path_dir_image_train = path + "image_train_complete/"
+path_dir_image_test = path + "image_test_complete/"
+path_data_train = path + 'ISB_Train_complete.csv'
+path_data_test = path + 'ISB_Test_complete.csv'
+num_class = 10
+
+# VALIDATE INPUT DATA
+assert os.path.exists(path), 'No existe el directorio de datos ' + path
+assert os.path.exists(path_data_train), 'No existe el archivo con los datos de entrenamiento ' + path_data_train
+assert os.path.exists(path_data_test), 'No existe el archivo con los datos de pruebas ' + path_data_test
+
+
+# Función, fase de test
+def test_model(net, sess_test, objData):
+
+    total = objData.total_images
+    count_success = 0
+    count_by_class = np.zeros([num_class, num_class])
+    prob_predicted = []
+
+    print('\n# PHASE: Test classification')
+    for i in range(3):
+
+        batch, label = objData.generate_batch()
+        prob, layer = sess_test.run([net.prob, net.relu6], feed_dict={vgg_batch: batch, train_mode: False})
+
+        # save output of a layer
+        # utils.save_layer_output(layer, label, name='Train_SNC4_relu6', dir='../data/features/')
+        # utils.save_layer_output_by_class(layer, label, name='Train_SNC4', dir='../data/features/')
+
+        # Acumulamos los aciertos de cada iteracion, para despues hacer un promedio
+        count, count_by_class, prob_predicted = utils.print_accuracy(label, prob, matrix_confusion=count_by_class, predicted=prob_predicted)
+        count_success = count_success + count
+        objData.next_batch_test()
+
+    # promediamos la precision total
+    accuracy_final = count_success/total
+    print('\n# STATUS: Confusion Matrix')
+    print(count_by_class)
+    print('    Success total: ', str(count_success))
+    print('    Accuracy total: ', str(accuracy_final))
+
+    return accuracy_final
+
+
+# Funcion, fase de entrenamiento
+def train_model(net, sess_train, objData, epoch):
+
+    print('\n# PHASE: Training model')
+    for ep in range(epoch):
+        print('\n     Epoch:', ep)
+        t0 = time.time()
+        cost_i = 0
+        for i in range(objData.total_batchs):
+            batch, label = objData.generate_batch()
+
+            # Generate the 'one hot' or labels
+            label = tf.one_hot([li for li in label], on_value=1, off_value=0, depth=num_class)
+            label = list(sess_train.run(label))
+            # Run training
+            t_start = time.time()
+            _, cost = sess_train.run([net.train, net.cost], feed_dict={vgg_batch: batch, vgg_label: label, train_mode: True})
+            t_end = time.time()
+            # Next slice batch
+            objData.next_batch()
+
+            cost_i = cost_i + cost
+            print("        > Minibatch: %d train on batch time: %7.3f seg." % (i, (t_end - t_start)))
+
+        t1 = time.time()
+        print("        Cost per epoch: ", cost_i)
+        print("        Time epoch: %7.3f seg." % (t1 - t0))
+        print("        Time per iteration: %7.3f seg." % ((t1 - t0) / epoch))
+
+
+
+
+
+if __name__ == '__main__':
+
+    path_load_weight = '../weight/vgg19.npy'
+    path_save_weight = '../weight/save_1.npy'
+    load_weight_fc = False
+
+    epoch = 4
+    mini_batch_train = 20
+    mini_batch_test = 30
+    learning_rate = 0.0005
+
+    size_layer_fc = 4096
+    accuracy = 0
+
+    # GENERATE DATA
+    data_train = Dataset(path_data=path_data_train, path_dir_images=path_dir_image_train, minibatch=mini_batch_train, cols=[0, 1], restrict=True)
+    data_test = Dataset(path_data=path_data_test, path_dir_images=path_dir_image_test, minibatch=mini_batch_test, cols=[0, 1], random=False)
+    # data_test = Dataset(path_data=path_data_train, path_dir_images=path_dir_image_train, minibatch=mini_batch_train, cols=[0, 1], random=False)
+
+    with tf.Session() as sess:
+
+        import tensorflow.examples.tutorials.mnist.input_data as input_data
+        mnist = input_data.read_data_sets("../data/MNIST_data/", one_hot=True)
+        trX, trY, teX, teY = mnist.train.images, mnist.train.labels, mnist.test.images, mnist.test.labels
+
+        # DEFINE MODEL
+        vgg_batch = tf.placeholder(tf.float32, [None, 784])
+        vgg_label = tf.placeholder(tf.float32, [None, num_class])
+        train_mode = tf.placeholder(tf.bool)
+
+        # Initialize of the model VGG19
+        alex = ALEX.VGG19(path_load_weight, learning_rate=learning_rate, load_weight_fc=load_weight_fc)
+        alex.build(vgg_batch, vgg_label, train_mode, size_layer_fc=size_layer_fc, num_class=10)
+        sess.run(tf.global_variables_initializer())
+        total = len(trX)
+        # Train red
+        for i in range(epoch):
+            for start, end in zip(range(0, total, 128), range(128, total, 128)):
+                print('[',start, end,']')
+                input_ = trX[start:end]
+                label_ = trY[start:end]
+                sess.run(alex.train, feed_dict={vgg_batch: input_, vgg_label: label_, train_mode:True})
+
+            print(i, sess.run(alex.cost, feed_dict={vgg_batch: mnist.test.images, vgg_label: mnist.test.labels, train_mode: False}))
+
+        # # Execute Network
+        # test_model(net=alex, sess_test=sess, objData=data_test)
+        # train_model(net=alex, sess_train=sess, objData=data_train, epoch=epoch)
+        # accuracy = test_model(net=alex, sess_test=sess, objData=data_test)
+        #
+        # # SAVE LOG: Genera un registro en el archivo log-server.txt
+        # utils.write_log(total_data=data_train.total_images,
+        #                 epoch=epoch,
+        #                 m_batch=mini_batch_train,
+        #                 l_rate=learning_rate,
+        #                 accuracy=accuracy,
+        #                 file_npy=path_load_weight,
+        #                 extra=str(size_layer_fc))
+        #
+        # # SAVE WEIGHTs
+        # vgg.save_npy(sess, path_save_weight)

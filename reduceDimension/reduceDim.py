78bdb932260a072aa1d47d86d04774ce31449771
Ricardo Coronado <rikardo.corp@gmail.com>
2017-06-30 21:22:33 -0500

    incremental PCA

--- reduceDimension/reduceDim.py
+++ reduceDimension/reduceDim.py
@@ -45,6 +45,7 @@ def path_datasets(opc):
         path_data_train = [xpath + data_name + '/' + 'mnist-train-800.csv']
         path_max = xpath + data_name + '/' +'max-mnist.csv'
         dims = [63,94,141]
+        method = 'pca'
 
     elif opc == 1:
         # CIFAR
@@ -54,6 +55,7 @@ def path_datasets(opc):
         path_data_train = [xpath + data_name + '/' + 'cifar10-train-4096.csv']
         path_max = xpath + data_name + '/' + 'max-cifar10.csv'
         dims = [94, 141, 211]
+        method = 'pca'
 
     elif opc == 2:
         # SVHN
@@ -63,6 +65,7 @@ def path_datasets(opc):
         path_data_train = [xpath + data_name + '/' + 'svhn-train-1152.csv']
         path_max = xpath + data_name + '/' + 'max-svhn.csv'
         dims = [42,63,94]
+        method = 'pca'
 
     elif opc == 3:
         # AGNews
@@ -72,8 +75,10 @@ def path_datasets(opc):
         path_data_train = [xpath + data_name + '/' + 'agnews-train-8704.csv']
         path_max = xpath + data_name + '/' + 'max-agnews.csv'
         dims = [94, 141, 211]
+        #dims = [141, 211, 316]
+        method = 'inpca'
 
-    return path_data_train, path_data_test, path_max, data_name, dims
+    return path_data_train, path_data_test, path_max, data_name, dims, method
 
 
 def reduce_dimension_function(option, X_train, new_dim):
@@ -83,6 +88,15 @@ def reduce_dimension_function(option, X_train, new_dim):
         pca = PCA(n_components=new_dim)
         pca.fit(X_train)
         X_reduced = pca.transform(X_train)
+        print(np.shape(X_reduced))
+        return X_reduced
+
+    elif option == 'inpca':
+        print('rick')
+        n_batches = new_dim
+        ipca = IncrementalPCA(n_components=new_dim, batch_size=n_batches)
+        ipca.fit(X_train)
+        X_reduced = ipca.transform(X_train)
         print(np.shape(X_reduced))
         return X_reduced
 
@@ -223,13 +237,15 @@ def make_reduce_matrix(path_data, xmethod, dim_optimal, dataname, extraname):
 
 if __name__ == '__main__':
 
-    for opc in range(4):
-        path_data_train_csv, path_data_test_csv, path_max_csv, name, dims = path_datasets(opc)
+    for opc in range(3, 4):
+        path_data_train_csv, path_data_test_csv, path_max_csv, name, dims, method = path_datasets(opc)
+        print('[', name, ']')
 
         for xdim in dims:
-            a = make_reduce_matrix(path_data_test_csv[0], 'pca', xdim, name, 'test')
-            b = make_reduce_matrix(path_data_train_csv[0], 'pca', xdim, name, 'train')
+            a = make_reduce_matrix(path_data_test_csv[0], method, xdim, name, 'test')
+            b = make_reduce_matrix(path_data_train_csv[0], method, xdim, name, 'train')
             utils.normalization_complete([a, b])
+
 
 
 
